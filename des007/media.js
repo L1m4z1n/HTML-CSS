48e0c376ca1d1db2fad0739d02b9af3b34c865fb
L1m4z1n <144862058+L1m4z1n@users.noreply.github.com>
2024-07-10 12:36:23 -0300

    Boletim completo

--- des007/media.js
+++ des007/media.js
@@ -1,7 +1,7 @@
 function media() {
     var avisomat= window.document.getElementById('aviso')
-   var n1= Number.parseInt(window.prompt('Digite sua Nota do primeiro semestre em matematica:'))
-   var n2= Number.parseInt(window.prompt('Digite sua segunda nota em matematica:'))
+   var n1= Number.parseInt(window.prompt('Digite sua Nota do primeiro semestre em matemática:'))
+   var n2= Number.parseInt(window.prompt('Digite sua segunda nota em matemática:'))
    var t1= window.document.getElementById('n1')
    var t2= window.document.getElementById('n2')
    var medmat=(n1+n2)/2
@@ -10,8 +10,8 @@ function media() {
     t1.innerHTML=(`${n1}`)
     t2.innerHTML=(`${n2}`)
    
-   var n1fis= Number.parseInt(window.prompt('Digite sua Nota do primeiro semestre em fisica:'))
-   var n2fis= Number.parseInt(window.prompt('Digite sua segunda nota em fisica:'))
+   var n1fis= Number.parseInt(window.prompt('Digite sua Nota do primeiro semestre em física:'))
+   var n2fis= Number.parseInt(window.prompt('Digite sua segunda nota em física:'))
    var f1=window.document.getElementById('n1fis')
    var f2=window.document.getElementById('n2fis')
    var medfis= (n1fis+n2fis)/2
@@ -45,7 +45,31 @@ function media() {
     
     mediatot.innerHTML=(`${mediaexatas}`)
 
-    if (mediaexatas<7) {
+    var n1his=Number.parseInt(window.prompt('Digite sua Nota do primeiro semestre em história:'))
+    var n2his=Number.parseInt(window.prompt('Digite sua segunda nota em história:'))
+    var h1= window.document.getElementById('n1his')
+    var h2=window.document.getElementById('n2his')
+    var medhis= (n1his+n2his)/2
+    var reshis= window.document.getElementById('reshis')
+    reshis.innerHTML=(`${medhis}`)
+    h1.innerHTML=(`${n1his}`)
+    h2.innerHTML=(`${n2his}`)
+
+    var n1geo=Number.parseInt(window.prompt('Digite sua Nota do primeiro semestre em geografia:'))
+    var n2geo=Number.parseInt(window.prompt('Digite sua segunda nota em geografia:'))
+    var g1= window.document.getElementById('n1geo')
+    var g2=window.document.getElementById('n2geo')
+    var medgeo= (n1geo+n2geo)/2
+    var resgeo= window.document.getElementById('resgeo')
+    resgeo.innerHTML=(`${medgeo}`)
+    g1.innerHTML=(`${n1geo}`)
+    g2.innerHTML=(`${n2geo}`)
+
+    var medhum = window.document.getElementById('mediahumanas')
+    var mediahumanas=(medhis+medgeo)/2
+    medhum.innerHTML=(`${mediahumanas}`)
+
+    if (mediaexatas<7 || mediahumanas<7) {
         avisomat.innerHTML=(`Sua média foi menor que 7, você está <strong>reprovado</strong>!`)
         } else {
     aviso.innerHTML=(`Você está aprovado!`)
